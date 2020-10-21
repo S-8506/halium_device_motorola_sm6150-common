sm6150-dev/android_device_xiaomi_sm6150-common

sm6150-common: Build ODM manifests with new proper way.
Bug: 72161927
Test: builds & boots

Change-Id: Ib30cd311f35ba7f8437f42862936a512b1e3b6e3

BoardConfigCommon.mk
@@ -118,6 +118,14 @@ TARGET_USES_QTI_MAPPER_EXTENSIONS_1_1 := true
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
+ODM_MANIFEST_SKUS += \
+	365 \
+	366 \
+	qva
+
+ODM_MANIFEST_365_FILES := $(COMMON_PATH)/manifest_sdmmagpie.xml
+ODM_MANIFEST_366_FILES := $(COMMON_PATH)/manifest_sdmmagpie.xml
+ODM_MANIFEST_QVA_FILES := $(COMMON_PATH)/manifest-qva.xml
 
 # Media
 TARGET_DISABLED_UBWC := true

S-8506/halium_device_motorola_sm6150-common

BoardConfigCommon.mk
@@ -105,6 +105,13 @@ TARGET_FS_CONFIG_GEN += \
     $(PLATFORM_PATH)/config.fs \
     $(PLATFORM_PATH)/mot_aids.fs
 
+ODM_MANIFEST_SKUS += \
+	365 \
+	366
+
+ODM_MANIFEST_365_FILES := $(PLATFORM_PATH)/manifest_sdmmagpie.xml
+ODM_MANIFEST_366_FILES := $(PLATFORM_PATH)/manifest_sdmmagpie.xml
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(PLATFORM_PATH):libinit_sm6150
 TARGET_RECOVERY_DEVICE_MODULES := libinit_sm6150